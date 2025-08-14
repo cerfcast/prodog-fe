9645dab8f5e9e508d00f93b80a6c310081d68818
Will Hawkins <hawkinsw@obs.cr>
2025-08-14 01:38:47 -0400

    Make Prodog Speak The Request Status
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
 import { useState } from "react";
 
+enum ProdogMood {
+  Happy = "Happy",
+  Sad = "Sad",
+  Calm = "Calm",
+}
+
+interface SpeechBubbleProps {
+  speech: string;
+  mood: ProdogMood;
+}
+
 function App() {
   const [files, setFiles] = useState<Array<File>>(new Array<File>());
-  const [speech, setSpeech] = useState<string>("Drop some PDFs in my bowl and I will concatenate them!");
+  const [speech, setSpeech] = useState<string>(
+    "I am Prodog, your PDF Concatenating Hero. Drop some PDFs in my bowl and I will concatenate them!",
+  );
   const [concatenateDisabled, setConcatenateDisabled] = useState<boolean>(true);
+  const [mood, setMood] = useState<ProdogMood>(ProdogMood.Calm);
+
+  function SpeechBubble(props: SpeechBubbleProps) {
+    const SpeechTypeColor: { [idx: string]: React.CSSProperties } = {
+      [ProdogMood.Calm]: { backgroundColor: "white", fontWeight: "bold" },
+      [ProdogMood.Happy]: {
+        backgroundColor: "rgb(125 192 121)",
+        fontWeight: "bolder",
+      },
+      [ProdogMood.Sad]: { backgroundColor: "#ffee8c", fontWeight: "bolder" },
+    };
+
+    return (
+      <div className="speechoutline">
+        <div className="speechbubblec">
+          <div
+            className="speechbubble"
+            style={SpeechTypeColor[props.mood]}
+            dangerouslySetInnerHTML={{ __html: props.speech }}
+          >
+          </div>
+          <div className="triangle" style={SpeechTypeColor[props.mood]}></div>
+        </div>
+        <div className="speechbubblealignment"></div>
+      </div>
+    );
+  }
 
   const handleNewDrop = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -15,9 +55,13 @@ function App() {
           ...Array.from({ length: newFiles.length }).map((_, v) => {
             return newFiles.item(v)!;
           }),
-        ]
+        ];
         setFiles(newFileList);
-        setSpeech("You have fed me " + newFileList.map((v) => v.name).join(", ") + ". When you are done feeding me, click Concatenate and I will go to work!");
+        setMood(ProdogMood.Calm);
+        setSpeech(
+          "You have fed me " + newFileList.map((v) => v.name).join(", ") +
+            ". When you are done feeding me, click Concatenate and I will go to work!",
+        );
         setConcatenateDisabled(false);
       }
     }
@@ -33,12 +77,20 @@ function App() {
       form.append(file.name, file, file.name);
     });
 
+    setFiles([]);
+    setConcatenateDisabled(true);
+
     try {
       console.log(`Starting to concatenate: ${form}!`);
       const result = await fetch(url, {
         method: "POST",
         body: form,
       });
+
+      setMood(ProdogMood.Happy);
+      setSpeech(
+        `I'm all done!! Enjoy your file! If you want me to do more work, feed me again.`,
+      );
 
       console.log(`Success concatenating: ${result}!`);
       const objectUrl = URL.createObjectURL(await result.blob());
@@ -48,6 +100,10 @@ function App() {
       a.click();
       URL.revokeObjectURL(objectUrl);
     } catch (e) {
+      setMood(ProdogMood.Happy);
+      setSpeech(
+        `I tried to upload your files to concatenate them, but failed: ${e}`,
+      );
       console.log(`Could not upload: ${e}!`);
     }
     for (const file of files!) {
@@ -58,13 +114,7 @@ function App() {
   return (
     <div className="container">
       <div className="prodog">
-        <div className="speechoutline">
-          <div className="speechbubblec">
-            <div className="speechbubble">I am <i>prodog</i> ... your PDF-concatenating hero! {speech}</div>
-            <div className="triangle"></div>
-            </div>
-          <div className="speechbubblealignment"></div>
-        </div>
+        <SpeechBubble speech={speech} mood={mood} />
         <div className="dropspot">
           <input
             id="files"
@@ -75,7 +125,12 @@ function App() {
           />
         </div>
       </div>
-      <button id="upload" type="button" onClick={handleUpload} disabled={concatenateDisabled}>
+      <button
+        id="upload"
+        type="button"
+        onClick={handleUpload}
+        disabled={concatenateDisabled}
+      >
         Concatenate!
       </button>
     </div>
